a901f686c6958c1b94ce47d1874aec4ed0d920bd
Dhanesh Phulphagar <41522314+darayn@users.noreply.github.com>
2023-04-25 00:17:39 +0530

    review riouters

--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,10 @@ const {
   adminGetAllProduct,
   getOneProduct,
   adminUpdateOneProduct,
-  adminDeleteOneProduct
+  adminDeleteOneProduct,
+  addReview,
+  deleteReview,
+  getOnlyReviewsForOneProduct
 } = require("../controllers/productController");
 const { isLoggedIn, customRole } = require("../middlewares/user");
 
@@ -14,6 +17,9 @@ const { isLoggedIn, customRole } = require("../middlewares/user");
 // user routes
 router.route("/products").get(getAllProduct);
 router.route("/product/:id").get(getOneProduct);
+router.route("/review").put(isLoggedIn, addReview);
+router.route("/review").delete(isLoggedIn, deleteReview);
+router.route("/reviews").get(isLoggedIn, getOnlyReviewsForOneProduct);
 
 // admin routes
 router
