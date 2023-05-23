467e0579daaa124bd147caefc67d703b73cc440a
Dhanesh Phulphagar <41522314+darayn@users.noreply.github.com>
2023-05-23 23:49:59 +0530

    refactor NO NO NO NO NO NO NO NO NO NO NO NO NO NO NO NO NO NO NO NO

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
-const express = require('express')
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 const app = express();
-const morgan = require('morgan')
-const cookieparser = require('cookie-parser')
-const fileUpload = require('express-fileupload')
+const morgan = require("morgan");
+const cookieparser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 //swaggers docs related
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // regular middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // cookies and file middleware
 
 app.use(cookieparser());
-app.use(fileUpload({
+app.use(
+  fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
-}));
+  })
+);
 
 // temp Check
-app.set('view engine', 'ejs')
-
+app.set("view engine", "ejs");
 
 // morgan middleware
-app.use(morgan('tiny'))
-
+app.use(morgan("tiny"));
 
 // importing all the routes
-const home = require('./routes/home')
-const user = require('./routes/user')
-const product = require('./routes/product')
-const payment = require('./routes/payment')
-
+const home = require("./routes/home");
+const user = require("./routes/user");
+const product = require("./routes/product");
+const payment = require("./routes/payment");
 
 // router middleware
 app.use("/api/v1", home);
@@ -46,11 +44,9 @@ app.use("/api/v1", user);
 app.use("/api/v1", product);
 app.use("/api/v1", payment);
 
-
-app.get("/signuptest", (req,res) => {
-    res.render('signupTest')
-})
-
+app.get("/signuptest", (req, res) => {
+  res.render("signupTest");
+});
 
 //exporting app.js
 module.exports = app;
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,251 +1,248 @@
-const Product = require("../models/product")
+const Product = require("../models/product");
 const BigPromise = require("../middlewares/bigPromise");
 const CustomError = require("../utils/customError");
 const cookieToken = require("../utils/cookieToken");
-const cloudinary = require('cloudinary');
+const cloudinary = require("cloudinary");
 const WhereClause = require("../utils/whereClause");
 const product = require("../models/product");
 
-exports.addProduct = BigPromise(async (req,res, next) => {
-    // images
-    let imageArray = []
+exports.addProduct = BigPromise(async (req, res, next) => {
+  // images
+  let imageArray = [];
 
-    if (!req.files) {
-        return next(new CustomError('Images are required', 401))
-    }
+  if (!req.files) {
+    return next(new CustomError("Images are required", 401));
+  }
 
-    if(req.files){
-        for (let index = 0; index < req.files.photos.length; index++) {
-            let result = await cloudinary.v2.uploader.upload(
-              req.files.photos[index].tempFilePath,
-              {
-                folder: "products",
-              }
-            );
-            imageArray.push({
-                secure_url: result.secure_url,
-                id: result.public_id,
-            });
-            
+  if (req.files) {
+    for (let index = 0; index < req.files.photos.length; index++) {
+      let result = await cloudinary.v2.uploader.upload(
+        req.files.photos[index].tempFilePath,
+        {
+          folder: "products",
         }
+      );
+      imageArray.push({
+        secure_url: result.secure_url,
+        id: result.public_id,
+      });
     }
+  }
 
-    req.body.photos = imageArray;
-    req.body.user = req.user.id;
+  req.body.photos = imageArray;
+  req.body.user = req.user.id;
 
-    const product = await Product.create(req.body);
-    res.status(200).json({
-        success: true,
-        product,
-    });
+  const product = await Product.create(req.body);
+  res.status(200).json({
+    success: true,
+    product,
+  });
 });
 
-exports.getAllProduct = BigPromise(async(req,res,next) => {
-    const resultPerPage = 6
-    const totalcountProduct = await Product.countDocuments()
+exports.getAllProduct = BigPromise(async (req, res, next) => {
+  const resultPerPage = 6;
+  const totalcountProduct = await Product.countDocuments();
 
+  const productsObj = new WhereClause(Product.find(), req.query)
+    .search()
+    .filter();
 
-    
-    const productsObj = new WhereClause(Product.find(), req.query).search().filter();
+  let products = await productsObj.base;
 
-    let products = await productsObj.base
+  const filteredProductNumber = products.length;
 
-    const filteredProductNumber = products.length
+  productsObj.pager(resultPerPage);
+  products = await productsObj.base.clone();
 
-    productsObj.pager(resultPerPage)
-    products = await productsObj.base.clone()
+  res.status(200).json({
+    success: true,
+    products: products,
+    filteredProductNumber: filteredProductNumber,
+    totalcountProduct: totalcountProduct,
+  });
+});
 
-    res.status(200).json({
-        success: true,
-        products: products,
-        filteredProductNumber: filteredProductNumber,
-        totalcountProduct: totalcountProduct
-    })
-})
+exports.getOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
-exports.getOneProduct = BigPromise(async(req,res,next) => {
-    const product = await Product.findById(req.params.id)
+  if (!product) {
+    return next(new CustomError("No Product found with this id", 401));
+  }
+  res.status(200).json({
+    success: true,
+    product: product,
+  });
+});
 
-    if (!product){
-        return next(new CustomError('No Product found with this id', 401))
+exports.addReview = BigPromise(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const AlreadyReview = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (AlreadyReview) {
+    product.reviews.forEach((review) => {
+      if (review.user.toString() === req.user._id.toString()) {
+        review.comment = comment;
+        review.rating = rating;
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numberOfReviews = product.reviews.length;
+  }
+
+  // adjust average rating
+
+  product.ratings =
+    product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    product.reviews.length;
+
+  // Save
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+exports.deleteReview = BigPromise(async (req, res, next) => {
+  const { productId } = req.query;
+
+  const product = await Product.findById(productId);
+
+  const reviews = product.reviews.filter(
+    (rev) => rev.user.toString() !== req.user._id.toString()
+  );
+
+  const numberOfReviews = reviews.length;
+
+  // adjust average rating
+  let ratings = 0;
+  if (numberOfReviews > 0) {
+    ratings =
+      reviews.reduce((acc, item) => item.rating + acc, 0) / numberOfReviews;
+  }
+
+  // update the product
+  await Product.findByIdAndUpdate(
+    productId,
+    {
+      reviews,
+      ratings,
+      numberOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
     }
-    res.status(200).json({
-        success: true,
-        product: product
-    })
-})
+  );
 
-exports.addReview = BigPromise(async(req,res,next) => {
-    const {rating, comment, productId} = req.body
-    const review = {
-        user: req.user._id,
-        name: req.user.name,
-        rating : Number(rating),
-        comment
-    }
+  res.status(200).json({
+    success: true,
+  });
+});
 
-    const product = await Product.findById(productId)
+exports.getOnlyReviewsForOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
 
-    const AlreadyReview = product.reviews.find(
-        (rev) => rev.user.toString() === req.user._id.toString()
-    )
-    
-    if(AlreadyReview){
-
-        product.reviews.forEach((review)=>{
-            if(review.user.toString() === req.user._id.toString()){
-                review.comment = comment
-                review.rating = rating
-            }
-        })
-    }
-    else{
-        product.reviews.push(review)
-        product.numberOfReviews = product.reviews.length
-    }
-
-    // adjust average rating 
-
-    product.ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) /
-    product.reviews.length
-
-    // Save
-    await product.save({validateBeforeSave: false})
-
-
-    res.status(200).json({
-        success: true
-    })
-
- })
-
- exports.deleteReview = BigPromise(async(req,res,next) => {
-    
-    const { productId } = req.query
-
-    const product = await Product.findById(productId)
-
-    const reviews = product.reviews.filter(
-        (rev) => rev.user.toString() !== req.user._id.toString()
-    )
-
-    const numberOfReviews = reviews.length
-
-    // adjust average rating 
-    let ratings = 0;
-    if (numberOfReviews > 0) {
-        ratings = reviews.reduce((acc, item) => item.rating + acc, 0) / numberOfReviews;
-    }
-
-    // update the product
-    await Product.findByIdAndUpdate(productId, {
-        reviews,
-        ratings,
-        numberOfReviews
-    },{
-        new: true,
-        runValidators: true,
-        useFindAndModify: false
-    })
-
-    res.status(200).json({
-        success: true
-    })
-
- })
-
-exports.getOnlyReviewsForOneProduct = BigPromise(async(req,res,next) => {
-    const product = await Product.findById(req.query.id)
-
-    res.status(200).json({
-        success: true,
-        reviews: product.reviews
-    })
-
- })
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
 
 // Admin only controllers
 
-exports.adminGetAllProduct = BigPromise(async(req,res,next) => {
-    const products = await Product.find()
-    if(!products){
-        res.status(400).json({
-            success: false,
-            message: "No Products Found",
-            products
-        })
-    }
-    res.status(200).json({
-        success: true,
-        products
-    })
-})
+exports.adminGetAllProduct = BigPromise(async (req, res, next) => {
+  const products = await Product.find();
+  if (!products) {
+    res.status(400).json({
+      success: false,
+      message: "No Products Found",
+      products,
+    });
+  }
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
 
-exports.adminUpdateOneProduct = BigPromise(async(req,res,next) => {
-    
-    let product = await Product.findById(req.params.id)
+exports.adminUpdateOneProduct = BigPromise(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
 
-    if (!product){
-        return next(new CustomError('No Product found with this id', 401))
-    }
+  if (!product) {
+    return next(new CustomError("No Product found with this id", 401));
+  }
 
-    let imagesArray = []
+  let imagesArray = [];
 
-    if(req.files){
-        // destroy the existing image
-        for (let index = 0; index < product.photos.length; index++) {
-            const res = await cloudinary.v2.uploader.destroy(product.photos[index].id)
-        }
-
-        // upload and save the images
-
-        
-        if (!req.files) {
-          return next(new CustomError("Images are required", 401));
-        }
-
-        for (let index = 0; index < req.files.photos.length; index++) {
-            let result = await cloudinary.v2.uploader.upload(
-                req.files.photos[index].tempFilePath,
-                {
-                    folder: process.env.FOLDERNAME, //folder name in .env
-                }
-            );
-            imagesArray.push({
-                secure_url: result.secure_url,
-                id: result.public_id,
-            });
-        }
-        req.body.photos = imagesArray
-    }
-
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-        useFindAndModify: false
-    })
-    res.status(200).json({
-        success: true,
-        product
-    })
-})
-
-exports.adminDeleteOneProduct = BigPromise(async(req,res,next) => {
-    
-    const product = await Product.findById(req.params.id)
-
-    if (!product){
-        return next(new CustomError('No Product found with this id', 401))
-    }
-
+  if (req.files) {
     // destroy the existing image
     for (let index = 0; index < product.photos.length; index++) {
-        const res = await cloudinary.v2.uploader.destroy(product.photos[index].id)
-    }    
-    
-    await product.remove()
-    res.status(200).json({
-        success: true,
-        message: "Product was deleted"
-    })
-})
+      const res = await cloudinary.v2.uploader.destroy(
+        product.photos[index].id
+      );
+    }
+
+    // upload and save the images
+
+    if (!req.files) {
+      return next(new CustomError("Images are required", 401));
+    }
+
+    for (let index = 0; index < req.files.photos.length; index++) {
+      let result = await cloudinary.v2.uploader.upload(
+        req.files.photos[index].tempFilePath,
+        {
+          folder: process.env.FOLDERNAME, //folder name in .env
+        }
+      );
+      imagesArray.push({
+        secure_url: result.secure_url,
+        id: result.public_id,
+      });
+    }
+    req.body.photos = imagesArray;
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+exports.adminDeleteOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new CustomError("No Product found with this id", 401));
+  }
+
+  // destroy the existing image
+  for (let index = 0; index < product.photos.length; index++) {
+    const res = await cloudinary.v2.uploader.destroy(product.photos[index].id);
+  }
+
+  await product.remove();
+  res.status(200).json({
+    success: true,
+    message: "Product was deleted",
+  });
+});
