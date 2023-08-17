5763b539dc143184722511307372a44df8901ac7
Dhanesh Phulphagar <41522314+darayn@users.noreply.github.com>
2023-08-17 23:04:46 +0530

    add stock field to product

--- models/product.js
+++ models/product.js
@@ -1,94 +1,91 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    
-
-    name: {
+  name: {
+    type: String,
+    required: [true, "Please provide product name"],
+    trim: true,
+    maxlength: [120, "Product name should not be more than 120 characters"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please provide product price"],
+    maxlength: [6, "Product name should not be more than 5 digits"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please provide product description"],
+  },
+  photos: [
+    {
+      id: {
         type: String,
-        required: [true, 'Please provide product name'],
-        trim: true,
-        maxlength: [120, 'Product name should not be more than 120 characters']
-    },
-    price: {
-        type: Number,
-        required: [true, 'Please provide product price'],
-        maxlength: [6, 'Product name should not be more than 5 digits']
-    },
-    description: {
+        required: true,
+      },
+      secure_url: {
         type: String,
-        required: [true, 'Please provide product description'],
+        required: true,
+      },
     },
-    photos: [
-        {
-            id: {
-                type: String,
-                required: true,
-            },
-            secure_url: {
-                type: String,
-                required: true,
-            },
-        },
+  ],
+  category: {
+    type: String,
+    required: [
+      true,
+      "Please select category from short-sleeves, long-sleeves, sweat-shirts, hoodies",
     ],
-    category: {
-        type: String,
-        required: [true, 'Please select category from short-sleeves, long-sleeves, sweat-shirts, hoodies'],
-        enum:{
-            values: [
-                'shortsleeves',
-                'longsleeves',
-                'sweatshirt',
-                'hoodies'
-            ],
-            message: "Please select category ONLY from short-sleeves, long-sleeves, sweat-shirts, hoodies"
-        }
+    enum: {
+      values: ["shortsleeves", "longsleeves", "sweatshirt", "hoodies"],
+      message:
+        "Please select category ONLY from short-sleeves, long-sleeves, sweat-shirts, hoodies",
     },
-    brand: {
-        type: String,
-        required: [true, 'Please add a brand for clothing']
-    },
-    ratings : {
-        type: Number,
-        default: 0
-    },
-    numberOfReviews: {
-        type: Number,
-        default: 0
-    },
-    reviews: [
-        {
-            user: {
-                type: mongoose.Schema.ObjectId,
-                ref: 'User',
-                required: true
-            },
-            name: {
-                type: String,
-                required: true
-            },
-            rating: {
-                type: Number,
-                required: true
-            },
-            comment:{
-                type: String,
-                required: true
-            }
-        }
-    ],
-    user: {
+  },
+  stock: {
+    type: Number,
+    required: [true, "please add a number a number in stock"],
+  },
+  brand: {
+    type: String,
+    required: [true, "Please add a brand for clothing"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  numberOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
         type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: true
+        ref: "User",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    }
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-
-
-})
-
-
-module.exports = mongoose.model('Product', productSchema )
+module.exports = mongoose.model("Product", productSchema);
